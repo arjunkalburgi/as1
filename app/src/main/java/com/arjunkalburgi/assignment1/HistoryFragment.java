dda9356a1880d61d3b13ab6de76798aef443cc11
Arjun Kalburgi <askalburgi@gmail.com>
2016-09-23 15:52:39 -0600

    Adding Fragments from https://guides.codepath.com/android/ViewPager-with-FragmentPagerAdapter

---
+++ app/src/main/java/com/arjunkalburgi/assignment1/HistoryFragment.java
@@ -0,0 +1,44 @@
+package com.arjunkalburgi.assignment1;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Arjun on 2016-09-23.
+ */
+public class HistoryFragment extends Fragment {
+    // Store instance variables
+    private String title;
+    private int page;
+
+    // newInstance constructor for creating fragment with arguments
+//    public static HistoryFragment newInstance(int page, String title) {
+//        HistoryFragment fragmentFirst = new HistoryFragment();
+//        Bundle args = new Bundle();
+//        args.putInt("someInt", page);
+//        args.putString("someTitle", title);
+//        fragmentFirst.setArguments(args);
+//        return fragmentFirst;
+//    }
+
+    // Store instance variables based on arguments passed
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        page = getArguments().getInt("someInt", 0);
+        title = getArguments().getString("someTitle");
+    }
+
+    // Inflate the view for the fragment based on layout XML
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.history_fragment_layout, container, false);
+        //TextView tvLabel = (TextView) view.findViewById(R.id.tvLabel);
+        //tvLabel.setText(page + " -- " + title);
+        return view;
+    }
+}
